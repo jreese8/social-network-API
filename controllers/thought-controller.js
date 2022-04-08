cea0ec799a80ede2924882e0547925a4252c5a6c
Johnathan Reese <JohnathanReese8@gmail.com>
2022-04-08 17:59:26 -0400

    Finished up thought-controller. Referenced pizza-hunt

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -30,6 +30,75 @@ const thoughtController = {
   addThought(req, res) {
     Thought.create(req.body)
     .then((dbThoughtData) => {
-      
+      res.json(dbThoughtData);
     })
-  }
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  },
+
+  updateThought({params, body}, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.id }, body, 
+      { new: true, runValidators: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: 'No thought assigned to this ID' });
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+    },
+
+    deleteThought({params, body}, res) {
+      Thought.findOneAndDelete(
+        { _id: params.id } //, body, 
+        // { new: true, runValidators: true }
+      )
+        .then((dbThoughtData) => {
+          if (!dbThoughtData) {
+            return res.status(404).json({ message: 'No thought assigned to this ID' });
+          }
+          res.json(dbThoughtData);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+      },
+
+      addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+          { _id: params.thoughtId }, 
+          { $push: { reactions: body } },
+          { new: true, runValidators: true }
+        )
+        .then((dbThoughtData) => {
+          if (!dbThoughtData) {
+            return res.status(404).json({ message: 'No thought assigned to this ID' });
+          }
+          res.json(dbThoughtData);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+      },
+
+      deleteReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+          { _id: params.thoughtId },
+          { $pull: { reaction: { reactionId: body.reactionId } } },
+          { new: true }
+        )
+        .then((dbThoughtData) => res.json(dbThoughtData))
+        .catch((err) => res.json(err));
+        },
+      };
+
+      module.exports = thoughtController;
