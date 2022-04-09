781c7d80f173f8e2eb01cd17b454fd1a7a5e39e8
Johnathan Reese <JohnathanReese8@gmail.com>
2022-04-08 22:11:49 -0400

    Edited server.js

--- server.js
+++ server.js
@@ -4,81 +4,19 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const db = require('./models');
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/populatedb',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
+// Use to log mongo queries that are executed
 mongoose.set('debug', true);
 
-// A user has been created already for our activity purposes
-db.User.create({ name: 'Ernest Hemingway' })
-  .then(dbUser => {
-    console.log(dbUser);
-  })
-  .catch(({ message }) => {
-    console.log(message);
-  });
+app.use(require('./routes'));
 
-// Retrieve all notes
-app.get('/notes', (req, res) => {
-  db.Note.find({})
-    .then(dbNote => {
-      res.json(dbNote);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-// Retrieve all users
-app.get('/user', (req, res) => {
-  db.User.find({})
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-// Create a new note and associate it with user
-app.post('/submit', ({ body }, res) => {
-  db.Note.create(body)
-    .then(({ _id }) =>
-      db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true })
-    )
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.get('/populate', (req, res) => {
-  db.User.find({})
-    .populate({
-      path: 'notes',
-      select: '-__v'
-    })
-    .select('-__v')
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
