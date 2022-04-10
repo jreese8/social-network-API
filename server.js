afc29656365caa18ab2329080a046a6891bf6a08
Johnathan Reese <JohnathanReese8@gmail.com>
2022-04-09 22:26:33 -0400

    removed UseFindAndModify from server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-API', {
-  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
